693311212a085b394f1eae4471c1ad7b045163cc
noahsadir <development.noahsadir@gmail.com>
2021-06-24 12:00:03 -0400

    initial commit

---
+++ KeepTrack-iOS/KeepTrack-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KeepTrack-iOS
+//
+//  Created by Noah Sadir on 6/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
